ae7d258aab4de7f92ddc6014f5a3d35f2a4a0b6e
lara <larafercue@gmail.com>
2019-07-29 10:57:52 +0200

    Adding ranged weapons and projectiles

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Projectile : MonoBehaviour
+    {
+        public float speed = 10.0f;
+        public float timeToLive = 600.0f;
+        public int damage;
+        public string targetTag;
+
+        // Start is called before the first frame update
+        void OnEnable()
+        {
+            GetComponent<Rigidbody>().velocity = transform.forward * speed;
+            Destroy(gameObject, timeToLive);
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Character character = other.GetComponent<Character>();
+            if (character != null && other.transform.gameObject.tag == targetTag)
+            {
+                Destroy(gameObject);
+                character.ApplyDamage(damage);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/RangedWeapon.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets.Scripts {
+    public class RangedWeapon : Weapon
+    {
+        public GameObject projectile;
+
+        public override void Damage(GameObject target)
+        {
+            Quaternion rotation = new Quaternion(transform.rotation.x, transform.rotation.y, transform.rotation.z, transform.rotation.w);
+            GameObject prefab = Instantiate(this.projectile, transform.position, rotation);
+            Projectile projectile = prefab.GetComponent<Projectile>();
+            projectile.damage = Random.Range(minDamage, maxDamage);
+            projectile.targetTag = target.tag;
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            Attack(other);
+        }
+
+        private void OnTriggerStay(Collider other)
+        {
+            Attack(other);
+        }
+    }
+}
