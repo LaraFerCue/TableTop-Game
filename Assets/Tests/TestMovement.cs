a95725d61048b4b1ec2537359d7c2dab9744e874
lara <larafercue@gmail.com>
2019-08-08 10:25:35 +0200

    Adding messages to the asserts

--- Assets/Tests/TestMovement.cs
+++ Assets/Tests/TestMovement.cs
@@ -19,9 +19,9 @@ namespace Tests
 
             movement.Destination = new Vector3(1.0f, 0.0f, 4.0f);
 
-            Assert.That(movement.Destination.x == 1.0f);
-            Assert.That(movement.Destination.y == 0.0f);
-            Assert.That(movement.Destination.z == 4.0f);
+            Assert.That(movement.Destination.x == 1.0f, "The X component is " + movement.Destination.x);
+            Assert.That(movement.Destination.y == 0.0f, "The Y component is " + movement.Destination.y);
+            Assert.That(movement.Destination.z == 4.0f, "The Z component is " + movement.Destination.z);
             Assert.That(movement.IsMoving);
         }
 
@@ -33,9 +33,9 @@ namespace Tests
 
             movement.Direction = new Vector3(1.0f, 0.0f, 4.0f);
 
-            Assert.That(movement.Direction.x == 1.0f);
-            Assert.That(movement.Direction.y == 0.0f);
-            Assert.That(movement.Direction.z == 4.0f);
+            Assert.That(movement.Direction.x == 1.0f, "The X component is " + movement.Direction.x);
+            Assert.That(movement.Direction.y == 0.0f, "The Y component is " + movement.Direction.y);
+            Assert.That(movement.Direction.z == 4.0f, "The Z component is " + movement.Direction.z);
             Assert.That(movement.IsRotating);
         }
 
@@ -45,8 +45,9 @@ namespace Tests
             GameObject pawn = new GameObject();
             Movement movement = pawn.AddComponent<Movement>();
             movement.speed = 1.0f;
+            Vector3 finalPoint = new Vector3(1.0f, 0.0f, 4.0f);
 
-            movement.Destination = new Vector3(1.0f, 0.0f, 4.0f);
+            movement.Destination = finalPoint;
 
             int seconds = 0;
             while (movement.IsMoving && seconds < 10)
@@ -54,7 +55,9 @@ namespace Tests
                 movement.MoveTorwards(1.0f);
                 seconds++;
             }
-            Assert.That(Vector3.Distance(pawn.transform.position, new Vector3(1.0f, 0.0f, 4.0f)) < 0.1f);
+            Assert.That(Vector3.Distance(pawn.transform.position, finalPoint) < 0.1f,
+                "The distance to the point is " + Vector3.Distance(pawn.transform.position, finalPoint) +
+                " Position: " + pawn.transform.position);
         }
 
         [Test]
@@ -63,8 +66,10 @@ namespace Tests
             GameObject pawn = new GameObject();
             Movement movement = pawn.AddComponent<Movement>();
             movement.angularSpeed = 1.0f;
+            Vector3 pointToLook = new Vector3(1.0f, 0.0f, 4.0f);
+            Quaternion finalDirection = Quaternion.LookRotation(pointToLook.normalized);
 
-            movement.Direction = new Vector3(1.0f, 0.0f, 4.0f);
+            movement.Direction = pointToLook;
 
             int seconds = 0;
             while (movement.IsRotating && seconds < 10)
@@ -72,7 +77,8 @@ namespace Tests
                 movement.LookTorwards(1.0f);
                 seconds++;
             }
-            Assert.That(Quaternion.Angle(pawn.transform.rotation, new Quaternion(0.0f, 0.1f, 0.0f, 1.0f)) < 5.0f);
+            Assert.That(Quaternion.Angle(pawn.transform.rotation, finalDirection) < 5.0f,
+                "The rotation is not correct " + pawn.transform.rotation + " != " + finalDirection);
         }
     }
 }
